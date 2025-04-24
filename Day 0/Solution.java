9672144b2e873ab839196fce60e8fcd2016801da
João Lucas <jlmachado4004@gmail.com>
2025-04-24 08:36:31 -0400

    Added Day 0 of Hacker Rank Code Challenge

---
+++ Day 0/Solution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    String texto = scn.nextLine();
+
+
+    scn.close();
+
+    System.out.println("Hello, World.");
+    System.out.println(texto);
+  }
+}
