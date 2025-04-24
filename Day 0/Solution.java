4243d82effc1436c4aee8e68e903da0dc71c12f6
João Lucas <jlmachado4004@gmail.com>
2025-04-24 16:46:20 -0400

    Added Day 1 of Hacker Rank Code challenge

--- Day 0/Solution.java
+++ Day 0/Solution.java
@@ -1,18 +1,24 @@
-import java.io.*;
-import java.util.*;
 import java.util.Scanner;
 
 public class Solution {
-
   public static void main(String[] args) {
-    Scanner scn = new Scanner(System.in);
+    int i = 4;
+    double d = 4.0;
+    String s = "HackerRank ";
 
-    String texto = scn.nextLine();
+    Scanner scan = new Scanner(System.in);
 
+    int intInput = scan.nextInt();
+    double doubleInput = scan.nextDouble();
+    scan.nextLine();
+    String stringInput = scan.nextLine();
 
-    scn.close();
+    System.out.println(i + intInput);
 
-    System.out.println("Hello, World.");
-    System.out.println(texto);
+    System.out.printf("%.1f\n", d + doubleInput);
+
+    System.out.println(s + stringInput);
+
+    scan.close();
   }
 }
